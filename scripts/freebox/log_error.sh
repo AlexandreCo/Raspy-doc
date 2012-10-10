7f403796186aa7ff277d4b46f302a2be2ef74718
AlexandreCo <pi@raspberrypi.(none)>
2012-10-10 17:07:06 +0200

    ajout script de log des probleme de cx freebox

---
+++ scripts/freebox/log_error.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+LOG_FILE=/home/pi/Documents/logs/ping
+STATE=`cat $LOG_FILE`
+if ! ping -c 1 www.google.fr >/dev/null
+then 
+	echo "ko=$STATE"
+	if [ "$STATE" != "ko" ]
+	then
+		echo "ko" > $LOG_FILE
+		wget "http://127.0.0.1/emoncms3/emoncms3/api/post?apikey=be257fcfd02cf344e99ef6a4fcad9c7b&node=666&json={ping:0}"  -qO - >/dev/null
+	fi
+else
+	echo "ok=$STATE"
+        if [ "$STATE" != "ok" ]
+	then    
+		echo "ok" > $LOG_FILE
+		wget "http://127.0.0.1/emoncms3/emoncms3/api/post?apikey=be257fcfd02cf344e99ef6a4fcad9c7b&node=666&json={ping:1}"  -qO - >/dev/null
+	fi      
+fi
