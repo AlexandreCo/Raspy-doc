07353da0c9e24a95947e793d08770fc6ffb39e59
AlexandreCo <pi@raspberrypi>
2015-05-16 21:34:16 +0200

    add rain and Temp config

--- scripts/rf2txt/meteo.sh
+++ scripts/rf2txt/meteo.sh
@@ -1,31 +1,36 @@
 #!/bin/sh
+# mise a jour toutes les jour
+#script nb_jour num_capteur code
+# num capteur : 
+# pluie    : 198
+# Temp ext : 111
+# humiditée: 21
+# pression : 21
+#code :
+#  Température 1 : 4	
+#  Pression      : 5	
+#  Humidité	     : 6
+#  Température 2 : 7	
+#  Batterie	     : 8
+#  Rain          : 9
+
 #temperature
-echo "temperature"
-#/home/pi/Documents/scripts/rf2txt/display.sh 1 21 7 red
-#/home/pi/Documents/scripts/rf2txt/display.sh 1 0 4 red
-/home/pi/Documents/scripts/rf2txt/display.sh 1 11 4 red
+echo "temperature exterieur"
+/home/pi/Documents/scripts/rf2txt/display.sh 7 111 4 red > /dev/null
+/home/pi/Documents/scripts/rf2txt/display.sh 30 111 4 red > /dev/null
+/home/pi/Documents/scripts/rf2txt/display.sh 365 111 4 red > /dev/null
 
-#/home/pi/Documents/scripts/rf2txt/display.sh 7 21 7 red
-#/home/pi/Documents/scripts/rf2txt/display.sh 7 0 4 red
-/home/pi/Documents/scripts/rf2txt/display.sh 7 11 4 red
-
-#/home/pi/Documents/scripts/rf2txt/display.sh 30 21 7 red
-#/home/pi/Documents/scripts/rf2txt/display.sh 30 0 4 red
-/home/pi/Documents/scripts/rf2txt/display.sh 30 11 4 red
-
-#/home/pi/Documents/scripts/rf2txt/display.sh 365 21 7 red
-#/home/pi/Documents/scripts/rf2txt/display.sh 365 0 4 red
-/home/pi/Documents/scripts/rf2txt/display.sh 365 11 4 red
+echo "temperature exterieur (soleil)"
+/home/pi/Documents/scripts/rf2txt/display.sh 7 198 4 red > /dev/null
+/home/pi/Documents/scripts/rf2txt/display.sh 30 198 4 red > /dev/null
+/home/pi/Documents/scripts/rf2txt/display.sh 365 198 4 red > /dev/null
 
 #humiditée
-echo "humiditee"
-#/home/pi/Documents/scripts/rf2txt/display.sh 1 21 6 blue
-#/home/pi/Documents/scripts/rf2txt/display.sh 7 21 6 blue
-#/home/pi/Documents/scripts/rf2txt/display.sh 30 21 6 blue
-#/home/pi/Documents/scripts/rf2txt/display.sh 365 21 6 blue
+#/home/pi/Documents/scripts/rf2txt/display.sh 1 21 6 blue >  /dev/null
 #pression
-echo "pression"
-/home/pi/Documents/scripts/rf2txt/display.sh 1 11 5 green
-/home/pi/Documents/scripts/rf2txt/display.sh 7 11 5 green
-/home/pi/Documents/scripts/rf2txt/display.sh 30 11 5 green
-/home/pi/Documents/scripts/rf2txt/display.sh 365 11 5 green
+#/home/pi/Documents/scripts/rf2txt/display.sh 1 21 5 green >  /dev/null
+#rain
+echo "rain"
+/home/pi/Documents/scripts/rf2txt/display.sh 7 198 9 blue  >  /dev/null
+/home/pi/Documents/scripts/rf2txt/display.sh 30 198 9 blue  >  /dev/null
+/home/pi/Documents/scripts/rf2txt/display.sh 395 198 9 blue  >  /dev/null
--- scripts/rf2txt/meteo_h.sh
+++ scripts/rf2txt/meteo_h.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
+# mise a jour toutes les heures
+#script nb_jour num_capteur code
+# num capteur : 
+# pluie    : 198
+# Temp ext : 111
+# humiditée: 21
+# pression : 21
+#code :
+#  Température 1 : 4	
+#  Pression      : 5	
+#  Humidité	     : 6
+#  Température 2 : 7	
+#  Batterie	     : 8
+#  Rain          : 9
+
 #temperature
-/home/pi/Documents/scripts/rf2txt/display.sh 1 11 4 red > /dev/null
-#/home/pi/Documents/scripts/rf2txt/display.sh 1 21 7 red >  /dev/null
-#/home/pi/Documents/scripts/rf2txt/display.sh 1 0 4 red >  /dev/null
+/home/pi/Documents/scripts/rf2txt/display.sh 1 198 4 red > /dev/null
+/home/pi/Documents/scripts/rf2txt/display.sh 1 111 4 red > /dev/null
 #humiditée
 #/home/pi/Documents/scripts/rf2txt/display.sh 1 21 6 blue >  /dev/null
 #pression
 #/home/pi/Documents/scripts/rf2txt/display.sh 1 21 5 green >  /dev/null
+#rain
+/home/pi/Documents/scripts/rf2txt/display.sh 1 198 9 blue  >  /dev/null
